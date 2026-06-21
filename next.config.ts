d39f04f1a95a90884ad04aae0c7605267c95bdf2
Sarwan Yadav <sarwanyadav6174@gmail.com>
2026-06-21 14:01:10 +0530

    UPDATE
    ADDED OPTIMISATION FOR BUILD

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,20 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
     ignoreBuildErrors: true,
   },
+
   images: {
     remotePatterns: [
       {
         protocol: "https",
         hostname: "res.cloudinary.com",
-        port: "",
         pathname: "/**",
       },
     ],
   },
+
+  compress: true,
 };
 
 export default nextConfig;
